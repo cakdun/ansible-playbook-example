6d6ca4966e35f3cdb346ca34fe7179656a9b340b
cakdun <isdansitopu@gmail.com>
2024-01-06 22:45:31 +0700

    add host ip 192.168.1.104

--- script/sync.py
+++ script/sync.py
@@ -2,6 +2,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 awx_url = 'http://192.168.1.100/api/v2/'
+
 templateId = 18
 awx_username = 'nofrisdan'
 awx_password = 'N03d0600'
@@ -9,15 +10,17 @@ awx_password = 'N03d0600'
 def launch_job():
     awx_job_url = awx_url+"workflow_job_templates/"+str(templateId)+"/launch/"
     headers = {
-        'Content-Type':'application/json'
+        'Content-Type': 'application/json'
     }
 
     # launch
-    response = requests.post(awx_job_url,headers,verify=False,auth=HTTPBasicAuth(awx_username,awx_password)).json()
+    response = requests.post(awx_job_url,auth=HTTPBasicAuth(awx_username,awx_password)).json()
 
-    JobId = response.get("id")
+    print(response)
 
-    print("Job Success run in ID:",JobId)
+    # JobId = response.get("id")
+
+    # print("Job Success run in ID:",JobId)
 
 
 
