16e1f62752163c0d7e8c9ced495a87228175399f
Nofrisdan <nofrisdansitopu22@gmail.com>
2024-01-06 22:14:40 +0700

    menghapus host ip 192.168.1.103

---
+++ script/sync.py
@@ -0,0 +1,25 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+awx_url = 'http://192.168.1.100/api/v2/'
+templateId = 18
+awx_username = 'nofrisdan'
+awx_password = 'N03d0600'
+
+def launch_job():
+    awx_job_url = awx_url+"workflow_job_templates/"+str(templateId)+"/launch/"
+    headers = {
+        'Content-Type':'application/json'
+    }
+
+    # launch
+    response = requests.post(awx_job_url,headers,verify=False,auth=HTTPBasicAuth(awx_username,awx_password)).json()
+
+    JobId = response.get("id")
+
+    print("Job Success run in ID:",JobId)
+
+
+
+if __name__ == "__main__":
+    launch_job()
